d29526f9d36aab8709d3147669412e7452bbf9ec
nidhin <nagdeotenidhi@gmail.com>
2022-12-17 18:00:03 +0530

    hi

--- src/test/java/demo/Sample.java
+++ src/test/java/demo/Sample.java
@@ -13,6 +13,18 @@ public class Sample {
 	@Test
 	public void sampleDemo2()
 	{
-		System.out.println("This is a second sample");
+		System.out.println("This is a second sample 2");
+	}
+	
+	@Test
+	public void sampleDemo3()
+	{
+		System.out.println("This is a second sample 3");
+	}
+	
+	@Test
+	public void newSample()
+	{
+		System.out.println("This is new sample");
 	}
 }
